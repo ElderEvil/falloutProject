1d8e6b4d35d008e6a02aa429e1f68775c2280653
Dmytro <lordofelderevil@gmail.com>
2023-04-19 12:18:16 +0300

    :construction:Removed additional route level

--- app/api/endpoints/api.py
+++ app/api/endpoints/api.py
@@ -3,6 +3,6 @@ from fastapi import APIRouter
 from app.api.endpoints import junk, outfit, weapon
 
 api_router = APIRouter()
-api_router.include_router(junk.router, prefix="/junk", tags=["junk"])
-api_router.include_router(outfit.router, prefix="/outfits", tags=["outfits"])
-api_router.include_router(weapon.router, prefix="/weapons", tags=["weapons"])
+api_router.include_router(junk.router, tags=["junk"])
+api_router.include_router(outfit.router, tags=["outfits"])
+api_router.include_router(weapon.router, tags=["weapons"])

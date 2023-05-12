4bcb316e03df2498aea693d6d13444eef0141d93
Dmytro <lordofelderevil@gmail.com>
2023-05-13 00:58:09 +0300

    :white_check_mark:Updated junk tests

--- app/tests/test_api/conftest.py
+++ app/tests/test_api/conftest.py
@@ -2,11 +2,16 @@ import random
 
 import pytest
 
+from app.schemas.common import JunkType
+from app.tests.utils.utils import random_lower_string
+
 
 @pytest.fixture()
 def junk_data():
     return {
-        "name": "Test Junk",
+        "name": random_lower_string(16).capitalize(),
         "rarity": random.choice(["Common", "Rare", "Legendary"]),  # noqa: S311
         "value": random.randint(1, 1000),  # noqa: S311
+        "junk_type": random.choice(list(JunkType)),  # noqa: S311
+        "description": random_lower_string(32),
     }
--- app/tests/test_api/test_junk.py
+++ app/tests/test_api/test_junk.py
@@ -1,6 +1,13 @@
+import random
+
+from sqlmodel import Session
 from starlette.testclient import TestClient
 
+from app import crud
 from app.core.config import settings
+from app.schemas.common import JunkType
+from app.schemas.junk import JunkCreate
+from app.tests.utils.utils import random_lower_string
 
 
 def test_create_junk(client: TestClient, junk_data: dict):
@@ -10,19 +17,61 @@ def test_create_junk(client: TestClient, junk_data: dict):
     assert data["name"] == junk_data["name"]
     assert data["rarity"] == junk_data["rarity"]
     assert data["value"] == junk_data["value"]
+    assert data["junk_type"] == junk_data["junk_type"]
+    assert data["description"] == junk_data["description"]
 
 
-def test_read_junk_list():
-    assert True
+def test_read_junk_list(client: TestClient, session: Session, junk_data: dict):
+    junk_data_2 = {
+        "name": random_lower_string(16).capitalize(),
+        "rarity": "Common",
+        "value": random.randint(1, 1000),  # noqa: S311
+        "junk_type": random.choice(list(JunkType)),  # noqa: S311
+        "description": random_lower_string(16),
+    }
+    junk_obj_1 = JunkCreate(**junk_data)
+    junk_obj_2 = JunkCreate(**junk_data_2)
+    crud.junk.create(session, junk_obj_1)
+    crud.junk.create(session, junk_obj_2)
+    r = client.get(f"{settings.API_V1_STR}/junk/")
+    assert r.status_code == 200
 
 
-def test_read_junk():
-    assert True
+def test_read_junk(client: TestClient, session: Session, junk_data: dict):
+    junk_obj = JunkCreate(**junk_data)
+    junk_item = crud.junk.create(session, junk_obj)
+    r = client.get(f"{settings.API_V1_STR}/junk/{junk_item.id}")
+    assert r.status_code == 200
+    data = r.json()
+    assert data["name"] == junk_data["name"]
+    assert data["rarity"] == junk_data["rarity"]
+    assert data["value"] == junk_data["value"]
+    assert data["junk_type"] == junk_data["junk_type"]
+    assert data["description"] == junk_data["description"]
 
 
-def test_update_junk():
-    assert True
+def test_update_junk(client: TestClient, session: Session, junk_data: dict):
+    r = client.post(f"{settings.API_V1_STR}/junk/", json=junk_data)
+    junk_item = r.json()
+    junk_new_data = {
+        "name": random_lower_string(16).capitalize(),
+        "rarity": "Legendary",
+        "value": random.randint(1, 1000),  # noqa: S311
+        "junk_type": random.choice(list(JunkType)),  # noqa: S311
+        "description": random_lower_string(16),
+    }
+    update_response = client.put(f"{settings.API_V1_STR}/junk/{junk_item['id']}", json=junk_new_data)
+    updated_junk = update_response.json()
+    assert update_response.status_code == 200
+    assert updated_junk["name"] == junk_new_data["name"]
+    assert updated_junk["rarity"] == junk_new_data["rarity"]
+    assert updated_junk["value"] == junk_new_data["value"]
+    assert updated_junk["junk_type"] == junk_new_data["junk_type"]
+    assert updated_junk["description"] == junk_new_data["description"]
 
 
-def test_delete_junk():
-    assert True
+def test_delete_junk(client: TestClient, session: Session, junk_data: dict):
+    create_response = client.post(f"{settings.API_V1_STR}/junk/", json=junk_data)
+    junk_item = create_response.json()
+    delete_response = client.delete(f"{settings.API_V1_STR}/junk/{junk_item['id']}")
+    assert delete_response.status_code == 204

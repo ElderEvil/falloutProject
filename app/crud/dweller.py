62ae437522a2320c408622a16ceb5cb186bb8f38
Dmytro <lordofelderevil@gmail.com>
2023-05-15 11:55:15 +0300

    New dweller methods

--- app/crud/dweller.py
+++ app/crud/dweller.py
@@ -1,13 +1,38 @@
+from sqlmodel import Session
+
 from app.crud.base import CRUDBase
 from app.models.dweller import Dweller
 from app.schemas.dweller import DwellerCreate, DwellerUpdate
 
 
 class CRUDDweller(CRUDBase[Dweller, DwellerCreate, DwellerUpdate]):
-    ...
+    def experience(self, dweller: Dweller) -> int:
+        return dweller.experience
 
-    def add_exp(self):
-        ...  # TODO: Add exp to dweller
+    def level(self, dweller: Dweller):
+        return dweller.level
+
+    def calculate_experience_required(self, dweller: Dweller) -> int:
+        return int(100 * 1.5**dweller.level)
+
+    def add_experience(self, db: Session, dweller: Dweller, amount: int):
+        dweller.experience += amount
+        experience_required = self.calculate_experience_required(dweller)
+        if dweller.experience >= experience_required:
+            dweller.level += 1
+            dweller.experience -= experience_required
+        db.add(dweller)
+        db.commit()
+        db.refresh(dweller)
+
+    def is_alive(self, dweller: Dweller) -> bool:
+        return dweller.health > 0
+
+    def reanimate(self, dweller: Dweller) -> None:
+        if self.is_alive(dweller):
+            error_message = "Cannot reanimate alive dweller"
+            raise ValueError(error_message)
+        dweller.health = dweller.max_health
 
 
 dweller = CRUDDweller(Dweller)

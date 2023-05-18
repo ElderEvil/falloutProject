b40b002593e6828f8e8ceb7e05eebb2be914f237
Dmytro <lordofelderevil@gmail.com>
2023-05-18 23:33:06 +0300

    :lock:Added permission check to vault endpoints

--- app/api/v1/endpoints/vault.py
+++ app/api/v1/endpoints/vault.py
@@ -7,54 +7,54 @@ from app.api import deps
 from app.db.base import get_session
 from app.models.user import User
 from app.models.vault import Vault
-from app.schemas.vault import VaultCreate, VaultRead, VaultReadWithUser, VaultUpdate
+from app.schemas.vault import VaultCreate, VaultRead, VaultUpdate
 
 router = APIRouter()
 
 
 @router.post("/", response_model=Vault, status_code=201)
 def create_vault(
-        vault_data: VaultCreate,
-        db: Session = Depends(get_session),
-        user: User = Depends(deps.get_current_active_user),
+    vault_data: VaultCreate,
+    db: Session = Depends(get_session),
+    user: User = Depends(deps.get_current_active_user),
 ):
     vault_data.user_id = user.id
     return crud.vault.create(db, vault_data)
 
 
-@router.get("/", response_model=list[VaultReadWithUser])
+@router.get("/", response_model=list[VaultRead])
 def read_vault_list(
-        skip: int = 0,
-        limit: int = 100,
-        db: Session = Depends(get_session),
-        user: User = Depends(deps.get_current_active_superuser),
+    skip: int = 0,
+    limit: int = 100,
+    db: Session = Depends(get_session),
+    user: User = Depends(deps.get_current_active_superuser),
 ):
     return crud.vault.get_multi(db, skip=skip, limit=limit)
 
 
 @router.get("/my", response_model=list[VaultRead])
 def read_my_vaults(
-        db: Session = Depends(get_session),
-        user: User = Depends(deps.get_current_active_user),
+    db: Session = Depends(get_session),
+    user: User = Depends(deps.get_current_active_user),
 ):
     return crud.vault.get_by_user_id(db, user_id=user.id)
 
 
-@router.get("/{vault_id}", response_model=VaultReadWithUser)
+@router.get("/{vault_id}", response_model=VaultRead)
 def read_vault(
-        vault_id: UUID4,
-        db: Session = Depends(get_session),
-        user: User = Depends(deps.get_current_active_superuser),
+    vault_id: UUID4,
+    db: Session = Depends(get_session),
+    user: User = Depends(deps.get_current_active_superuser),
 ):
     return crud.vault.get(db, vault_id)
 
 
 @router.put("/{vault_id}", response_model=VaultRead)
 def update_vault(
-        vault_id: UUID4,
-        vault_data: VaultUpdate,
-        db: Session = Depends(get_session),
-        user: User = Depends(deps.get_current_active_user),
+    vault_id: UUID4,
+    vault_data: VaultUpdate,
+    db: Session = Depends(get_session),
+    user: User = Depends(deps.get_current_active_user),
 ):
     vault = crud.vault.get(db, vault_id)
     if vault.user_id != user.id or not user.is_superuser:
@@ -64,9 +64,9 @@ def update_vault(
 
 @router.delete("/{vault_id}", status_code=204)
 def delete_vault(
-        vault_id: UUID4,
-        db: Session = Depends(get_session),
-        user: User = Depends(deps.get_current_active_user),
+    vault_id: UUID4,
+    db: Session = Depends(get_session),
+    user: User = Depends(deps.get_current_active_user),
 ):
     vault = crud.vault.get(db, vault_id)
     if vault.user_id != user.id or not user.is_superuser:

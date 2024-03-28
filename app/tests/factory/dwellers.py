70322ddae49baeeda19fc42750a30dd77a2c40d4
Nedavnii <nedavniid.@gmail.com>
2024-03-28 23:28:57 +0200

    :test_tube:Fixed remaining tests

--- app/tests/factory/dwellers.py
+++ app/tests/factory/dwellers.py
@@ -9,30 +9,25 @@ fake = Faker()
 
 
 def create_fake_dweller():
-    first_name = fake.first_name()
-    last_name = fake.last_name()
-    gender = random.choice(list(Gender))
     rarity = random.choice(list(Rarity))
-    level = random.randint(1, 50)
-    experience = random.randint(0, 1000)
-    max_health = random.randint(50, 1000)
-    health = random.randint(0, max_health)
-    happiness = random.randint(10, 100)
-    is_adult = random.choice([True, False])
+    max_health = random.randint(50, 1_000)
 
     stats = get_stats_by_rarity(rarity)
 
     return stats | {
-        "first_name": first_name,
-        "last_name": last_name,
-        "gender": gender,
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "is_adult": random.choice([True, False]),
+        "gender": random.choice(list(Gender)),
         "rarity": rarity,
-        "level": level,
-        "experience": experience,
-        "max_health": max_health,
-        "health": health,
-        "happiness": happiness,
-        "is_adult": is_adult,
+        "level": random.randint(1, 50),
+        "experience": random.randint(0, 1_000),
+        "max_health": random.randint(50, 1_000),
+        "health": random.randint(50, max_health),
+        "radiation": random.randint(0, 1_000),
+        "happiness": random.randint(10, 100),
+        "stimpack": random.randint(0, 15),
+        "radaway": random.randint(0, 15),
     }
 
 
--- app/tests/test_crud/test_dweller.py
+++ app/tests/test_crud/test_dweller.py
@@ -3,30 +3,48 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import crud
 from app.schemas.dweller import DwellerCreate
+from app.schemas.user import UserCreate
+from app.schemas.vault import VaultCreateWithUserID
 from app.tests.factory.dwellers import create_fake_dweller
+from app.tests.factory.users import create_fake_user
+from app.tests.factory.vaults import create_fake_vault
 
 
 @pytest.mark.asyncio
 async def test_create_dweller(async_session: AsyncSession) -> None:
+    user_data = create_fake_user()
+    user_in = UserCreate(**user_data)
+    user = await crud.user.create(async_session, obj_in=user_in)
+    vault_data = create_fake_vault()
+    vault_in = VaultCreateWithUserID(**vault_data, user_id=user.id)
+    vault = await crud.vault.create(async_session, obj_in=vault_in)
     dweller_data = create_fake_dweller()
     dweller_in = DwellerCreate(**dweller_data, vault_id=str(vault.id))
     dweller = await crud.dweller.create(async_session, obj_in=dweller_in)
     assert dweller.first_name == dweller_data["first_name"]
     assert dweller.last_name == dweller_data["last_name"]
+    assert dweller.is_adult == dweller_data["is_adult"]
     assert dweller.gender == dweller_data["gender"]
     assert dweller.rarity == dweller_data["rarity"]
     assert dweller.level == dweller_data["level"]
     assert dweller.experience == dweller_data["experience"]
     assert dweller.max_health == dweller_data["max_health"]
     assert dweller.health == dweller_data["health"]
+    assert dweller.radiation == dweller_data["radiation"]
     assert dweller.happiness == dweller_data["happiness"]
-    assert dweller.is_adult == dweller_data["is_adult"]
 
 
 @pytest.mark.asyncio
 async def test_read_dweller(async_session: AsyncSession):
-    dweller_data.update({"vault_id": vault.id})
-    dweller = await crud.dweller.create(async_session, obj_in=DwellerCreate(**dweller_data))
+    user_data = create_fake_user()
+    user_in = UserCreate(**user_data)
+    user = await crud.user.create(async_session, obj_in=user_in)
+    vault_data = create_fake_vault()
+    vault_in = VaultCreateWithUserID(**vault_data, user_id=user.id)
+    vault = await crud.vault.create(async_session, obj_in=vault_in)
+    dweller_data = create_fake_dweller()
+    dweller_in = DwellerCreate(**dweller_data, vault_id=str(vault.id))
+    dweller = await crud.dweller.create(async_session, obj_in=dweller_in)
     dweller_read = await crud.dweller.get(async_session, id=dweller.id)
     assert dweller_read
     assert dweller.first_name == dweller_read.first_name
@@ -35,13 +53,20 @@ async def test_read_dweller(async_session: AsyncSession):
 
 @pytest.mark.asyncio
 async def test_dweller_add_exp(async_session: AsyncSession):
+    user_data = create_fake_user()
+    user_in = UserCreate(**user_data)
+    user = await crud.user.create(async_session, obj_in=user_in)
+    vault_data = create_fake_vault()
+    vault_in = VaultCreateWithUserID(**vault_data, user_id=user.id)
+    vault = await crud.vault.create(async_session, obj_in=vault_in)
+    dweller_data = create_fake_dweller()
     dweller_data["experience"] = 0
     dweller_data["level"] = 1
     dweller_data["vault_id"] = vault.id
     dweller_in = DwellerCreate(**dweller_data)
     dweller = await crud.dweller.create(async_session, obj_in=dweller_in)
     assert dweller.experience == dweller_data["experience"]
-    await crud.dweller.add_experience(async_session, dweller=dweller, amount=10)
+    await crud.dweller.add_experience(async_session, dweller_obj=dweller, amount=10)
     assert dweller.experience == 10
     exp_amount = crud.dweller.calculate_experience_required(dweller=dweller)
     await crud.dweller.add_experience(async_session, dweller=dweller, amount=exp_amount)

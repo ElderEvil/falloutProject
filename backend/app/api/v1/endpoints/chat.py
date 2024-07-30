0a918e5f4f99131f83baacdb6b33443dbec520bc
Dmytro Nedavnii <=>
2024-07-31 01:28:35 +0300

    :alembic:Test CI

--- backend/app/api/v1/endpoints/chat.py
+++ backend/app/api/v1/endpoints/chat.py
@@ -194,7 +194,6 @@ async def generate_objectives(
 async def websocket_endpoint(
     websocket: WebSocket,
     dweller_id: UUID4,
-    # _: CurrentActiveUser,
     db_session: AsyncSession = Depends(get_async_session),
 ):
     await websocket.accept()

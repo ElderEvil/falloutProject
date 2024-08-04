6cdb49c7c8e83e5ff03184267bca0f7cfc1be77a
Dmytro Nedavnii <=>
2024-08-05 00:18:06 +0300

    :construction:WIP

--- backend/app/api/celery_task.py
+++ backend/app/api/celery_task.py
@@ -3,7 +3,7 @@ import time
 from app.core.celery import celery_app
 
 
-@celery_app.task(name="create_task")
+@celery_app.task(name="create_task1")
 def create_task(task_time: int):
     time.sleep(task_time)
     return True

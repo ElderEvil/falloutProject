317fa778c55368ae8c4a1243ccb71f7150f66a10
Dmytro Nedavnii <=>
2024-11-25 18:49:31 +0200

    Fixed tests

--- backend/app/tests/test_api/test_vault.py
+++ backend/app/tests/test_api/test_vault.py
@@ -14,15 +14,15 @@ pytestmark = pytest.mark.asyncio(scope="module")
 async def test_create_vault(
     async_client: AsyncClient,
     async_session: AsyncSession,
-    normal_user_token_headers: dict[str, str],
+    superuser_token_headers: dict[str, str],
 ):
-    user = await crud.user.get_by_email(async_session, email=settings.EMAIL_TEST_USER)
+    user = await crud.user.get_by_email(async_session, email=settings.FIRST_SUPERUSER_EMAIL)
     vault_data = create_fake_vault()
     vault_data["user_id"] = str(user.id)
-    response = await async_client.post("/vaults/", headers=normal_user_token_headers, json=vault_data)
+    response = await async_client.post("/vaults/", headers=superuser_token_headers, json=vault_data)
     assert response.status_code == 201
     response_data = response.json()
-    assert response_data["name"] == vault_data["name"]
+    assert response_data["number"] == vault_data["number"]
     assert response_data["bottle_caps"] == vault_data["bottle_caps"]
     assert response_data["happiness"] == vault_data["happiness"]
     assert response_data["user_id"] == vault_data["user_id"]
@@ -44,14 +44,14 @@ async def test_read_vault_list(
     response_data = response.json()
     assert len(response_data) == 2
     response_vault_1, response_vault_2 = response_data
-    if response_vault_1["name"] == vault_2_data["name"]:
+    if response_vault_1["number"] == vault_2_data["number"]:
         response_vault_2, response_vault_1 = response_data
-    assert response_vault_1["name"] == vault_1_data["name"]
+    assert response_vault_1["number"] == vault_1_data["number"]
     assert response_vault_1["bottle_caps"] == vault_1_data["bottle_caps"]
     assert response_vault_1["happiness"] == vault_1_data["happiness"]
     assert response_vault_1["user_id"] == vault_1_data["user_id"]
 
-    assert response_vault_2["name"] == vault_2_data["name"]
+    assert response_vault_2["number"] == vault_2_data["number"]
     assert response_vault_2["bottle_caps"] == vault_2_data["bottle_caps"]
     assert response_vault_2["happiness"] == vault_2_data["happiness"]
     assert response_vault_2["user_id"] == vault_2_data["user_id"]
@@ -72,7 +72,7 @@ async def test_read_vault(
     assert response.status_code == 200
     response_data = response.json()
     assert response_data["id"] == str(created_vault.id)
-    assert response_data["name"] == vault_data["name"]
+    assert response_data["number"] == vault_data["number"]
     assert response_data["bottle_caps"] == vault_data["bottle_caps"]
     assert response_data["happiness"] == vault_data["happiness"]
     assert response_data["user_id"] == vault_data["user_id"]
@@ -101,7 +101,7 @@ async def test_update_vault(
     assert update_response.status_code == 200
     update_response_data = update_response.json()
     assert update_response_data["id"] == str(created_vault.id)
-    assert update_response_data["name"] == new_vault_data["name"]
+    assert update_response_data["number"] == new_vault_data["number"]
     assert update_response_data["bottle_caps"] == new_vault_data["bottle_caps"]
     assert update_response_data["happiness"] == new_vault_data["happiness"]
     assert update_response_data["user_id"] == vault_data["user_id"]

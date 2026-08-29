b3bd6be94090e92c136a26b3a94ee04f7c23d5fb
Dmytro <elder.evil.dev@proton.me>
2026-08-29 18:08:22 +0300

    feat: add apprentice status section to training page

--- frontend/src/modules/progression/views/TrainingView.vue
+++ frontend/src/modules/progression/views/TrainingView.vue
@@ -5,6 +5,7 @@ import { Icon } from '@iconify/vue'
 import UProgressBar from '@/core/components/ui/UProgressBar.vue'
 import TrainingQueuePanel from '@/modules/progression/components/training/TrainingQueuePanel.vue'
 import TrainingRoomCard from '@/modules/progression/components/training/TrainingRoomCard.vue'
+import ApprenticeSection from '@/modules/progression/components/training/ApprenticeSection.vue'
 import SidePanel from '@/core/components/common/SidePanel.vue'
 import PageContentRail from '@/core/components/common/PageContentRail.vue'
 import { useVaultStore } from '@/modules/vault/stores/vault'
@@ -146,6 +147,8 @@ onMounted(async () => {
               />
             </div>
           </section>
+
+          <ApprenticeSection />
 
           <div class="w-full">
             <TrainingQueuePanel />

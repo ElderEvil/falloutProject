357a937d58cd8b27399d3440314dae41bdaa7d62
Dmytro <elder.evil.dev@proton.me>
2026-09-12 14:07:03 +0300

    feat(map): mark location viewed when its detail opens
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)

--- frontend/src/modules/map/views/MapView.vue
+++ frontend/src/modules/map/views/MapView.vue
@@ -34,6 +34,7 @@ function handleMarkerClick(
   if (payload.kind === 'location') {
     selectedLocation.value = payload.data
     selectedVaultMarker.value = null
+    mapStore.markLocationViewed(payload.data.id)
   } else {
     selectedLocation.value = null
     selectedVaultMarker.value = payload.data
